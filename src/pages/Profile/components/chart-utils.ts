87ba7b7a70fea68fc782e3a9bbf59c827f7182a0
Tales Pereira <42183799+talesdsp@users.noreply.github.com>
2020-06-16 00:10:01 -0300

    create chart patterns

---
+++ src/pages/Profile/components/chart-utils.ts
@@ -0,0 +1,68 @@
+import { amber, blue, cyan, deepOrange, lightGreen, pink, red } from "@material-ui/core/colors";
+
+export const chartFill = [
+  {
+    match: {
+      id: "Ruby",
+    },
+    id: "dots",
+  },
+  {
+    match: {
+      id: "C",
+    },
+    id: "dots",
+  },
+  {
+    match: {
+      id: "Go",
+    },
+    id: "dots",
+  },
+  {
+    match: {
+      id: "Python",
+    },
+    id: "dots",
+  },
+  {
+    match: {
+      id: "PHP",
+    },
+    id: "dots",
+  },
+  {
+    match: {
+      id: "CSS",
+    },
+    id: "dots",
+  },
+  {
+    match: {
+      id: "Scala",
+    },
+    id: "lines",
+  },
+  {
+    match: {
+      id: "Elixir",
+    },
+    id: "lines",
+  },
+  {
+    match: {
+      id: "JavaScript",
+    },
+    id: "lines",
+  },
+];
+
+export const colors = [
+  blue.A200,
+  pink.A200,
+  red.A400,
+  amber.A200,
+  cyan.A200,
+  lightGreen.A400,
+  deepOrange.A200,
+];
