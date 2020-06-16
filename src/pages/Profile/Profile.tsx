be7104f4a0a3d1fde407b66874f20d0039fbcb5d
Tales Pereira <42183799+talesdsp@users.noreply.github.com>
2020-06-16 00:02:06 -0300

    stateful profile component  w/ error treatment

---
+++ src/pages/Profile/Profile.tsx
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { ContributionSection, RepoSection, UserSection } from "./components";
+import { LanguagesActions } from "./store/languages/actions";
+import { ReposActions } from "./store/repos/actions";
+import { Repo } from "./store/repos/types";
+import { ApplicationState } from "./store/types";
+import { UserActions } from "./store/user/actions";
+
+const selector = (state: ApplicationState) => ({ ...state });
+
+function Profile({ match, history }) {
+  const [selectedRepo, setSelectedRepo] = useState<Repo>();
+
+  const { user, repos, languages, error } = useSelector(selector);
+
+  const dispatch = useDispatch();
+
+  const name = match.params.username;
+
+  useEffect(() => {
+    try {
+      if (error.error) throw error.message;
+      dispatch(UserActions.getAsyncData(name));
+      dispatch(ReposActions.getAsyncData(name));
+    } catch (e) {
+      alert(
+        `${error.error.message}\n${error.message}\nPlease reload the page, if error persists the rate limit is exceeded, try again later.`
+      );
+    }
+  }, [dispatch, error.error, error.message, name]);
+
+  useEffect(() => {
+    try {
+      if (error.error) return;
+      if (selectedRepo) {
+        dispatch(LanguagesActions.getAsyncData(name, selectedRepo.name));
+      }
+    } catch (e) {
+      alert(
+        `${error.error.message}\n${error.message}\nPlease reload the page, if error persists the rate limit is exceeded, try again later.`
+      );
+    }
+  }, [dispatch, error, name, selectedRepo]);
+
+  return (
+    <>
+      <UserSection user={user} />
+
+      <RepoSection
+        repos={repos}
+        languages={languages}
+        selectedRepo={selectedRepo}
+        setSelectedRepo={setSelectedRepo}
+      />
+
+      <ContributionSection username={name} />
+    </>
+  );
+}
+
+export default Profile;
