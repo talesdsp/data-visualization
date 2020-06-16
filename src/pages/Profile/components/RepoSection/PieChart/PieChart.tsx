54f7b59a59bc25458452283e9ab38f978e627bfb
Tales Pereira <42183799+talesdsp@users.noreply.github.com>
2020-06-16 00:13:20 -0300

    FIX: #2 - pie chart is missing

---
+++ src/pages/Profile/components/RepoSection/PieChart/PieChart.tsx
@@ -0,0 +1,70 @@
+import { ResponsivePie } from "@nivo/pie";
+import React from "react";
+import { chartFill, colors } from "../../chart-utils";
+
+const PieChart = ({ data }) => {
+  return (
+    <ResponsivePie
+      data={data}
+      margin={{ left: -70, top: 55, bottom: 55 }}
+      innerRadius={0.1}
+      padAngle={2}
+      cornerRadius={0}
+      colors={colors}
+      enableRadialLabels={false}
+      radialLabelsTextXOffset={6}
+      radialLabelsTextColor="#fff"
+      radialLabelsLinkOffset={0}
+      radialLabelsLinkDiagonalLength={8}
+      radialLabelsLinkHorizontalLength={14}
+      radialLabelsLinkStrokeWidth={2}
+      radialLabelsLinkColor={{ from: "color" }}
+      slicesLabelsTextColor="#000"
+      animate={true}
+      motionStiffness={90}
+      motionDamping={15}
+      defs={[
+        {
+          id: "dots",
+          type: "patternDots",
+          background: "inherit",
+          color: "rgba(255, 255, 255, 0.3)",
+          size: 4,
+          padding: 1,
+          stagger: true,
+        },
+        {
+          id: "lines",
+          type: "patternLines",
+          background: "inherit",
+          color: "rgba(255, 255, 255, 0.3)",
+          rotation: -45,
+          lineWidth: 6,
+          spacing: 16,
+        },
+      ]}
+      fill={chartFill}
+      legends={[
+        {
+          anchor: "right",
+          direction: "column",
+          itemWidth: 80,
+          itemHeight: 21,
+          itemTextColor: "#eee",
+          symbolSize: 18,
+          symbolShape: "circle",
+          effects: [
+            {
+              on: "hover",
+              style: {
+                itemTextColor: "#fff",
+              },
+            },
+          ],
+        },
+      ]}
+    />
+  );
+};
+
+export default PieChart;
