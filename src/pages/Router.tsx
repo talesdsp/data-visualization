51299f57141cf8f9b5cb3a749f6636d0687b8d48
Tales Pereira <42183799+talesdsp@users.noreply.github.com>
2020-06-17 23:35:24 -0300

    Add path to router

--- src/pages/Router.tsx
+++ src/pages/Router.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Provider } from "react-redux";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import { Home, Profile } from "./index";
+import { Financial, Home, Profile } from "./index";
 import store from "./Profile/store";
 
 const Router = () => {
@@ -15,13 +15,20 @@ const Router = () => {
         <Provider store={store}>
           <Route exact path="/github/:username" component={Profile} />
         </Provider>
+
+        <Route exact path="/financial" component={Financial} />
       </Switch>
     </BrowserRouter>
   );
 };
 
 const Root = () => {
-  return <a href="/github">github</a>;
+  return (
+    <>
+      <a href="/github">github</a>
+      <a href="/financial">financial</a>
+    </>
+  );
 };
 
 export default Router;
