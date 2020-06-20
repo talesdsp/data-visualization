bf90cd4dee9f41d62d3a0d768eeb717eed4afdb3
Tales Pereira <42183799+talesdsp@users.noreply.github.com>
2020-06-20 00:48:27 -0300

    make /financial components stateless

--- src/pages/Financial/Financial.tsx
+++ src/pages/Financial/Financial.tsx
@@ -1,15 +1,18 @@
-import { Container } from "@material-ui/core";
 import React, { useRef, useState } from "react";
-import { AddMovingAverage, CandleStick } from "./components";
-import { series } from "./data";
+import { Dashboard } from "./components";
+import HomeBroker from "./components/HomeBroker/HomeBroker";
+import { series1 } from "./data";
 import MovingAverage from "./models/moving_average";
 
 const Financial: React.FC = () => {
   const [intervalEMA, setEMA] = useState(0);
   const [intervalSMA, setSMA] = useState(0);
+  const [broker, setBroker] = useState(false);
+
+  const [chosenChart, setChosenChart] = useState(series1);
 
   const [chart, setChart] = useState(
-    new MovingAverage({ series, intervalEMA, intervalSMA }).series
+    new MovingAverage({ series: chosenChart, intervalEMA, intervalSMA }).series
   );
 
   const smaEl = useRef(null);
@@ -17,20 +20,60 @@ const Financial: React.FC = () => {
 
   const toggleSMA = () => {
     setSMA(+smaEl.current.value);
-    setChart(new MovingAverage({ series, intervalEMA, intervalSMA: +smaEl.current.value }).series);
+    setChart(
+      new MovingAverage({ series: chosenChart, intervalEMA, intervalSMA: +smaEl.current.value })
+        .series
+    );
   };
   const toggleEMA = () => {
     setEMA(+emaEl.current.value);
-    setChart(new MovingAverage({ series, intervalEMA: +emaEl.current.value, intervalSMA }).series);
+    setChart(
+      new MovingAverage({ series: chosenChart, intervalEMA: +emaEl.current.value, intervalSMA })
+        .series
+    );
+  };
+
+  const [dashboard, setDashboard] = useState<boolean>(true);
+  const [container, setContainer] = useState<any>("");
+
+  const [name, setName] = useState<any>("Elisa Montenegro");
+  const [email, setEmail] = useState<any>("contact@elisa.com");
+  const [, setPassword] = useState<any>("contact@elisa.com");
+
+  const handleClick = () => {
+    let id = document.activeElement.id;
+    if (id === "wallet") {
+      setDashboard(true);
+    } else {
+      setDashboard(false);
+      setContainer(id);
+    }
   };
 
   return (
-    <div style={{ background: "var(--black)", position: "relative", padding: "3px" }}>
-      <Container maxWidth="md">
-        <CandleStick chart={chart} />
-        <AddMovingAverage emaEl={emaEl} smaEl={smaEl} toggleEMA={toggleEMA} toggleSMA={toggleSMA} />
-      </Container>
-    </div>
+    <>
+      <Dashboard
+        container={container}
+        dashboard={dashboard}
+        handleClick={handleClick}
+        setBroker={setBroker}
+        setPassword={setPassword}
+        name={name}
+        setName={setName}
+        email={email}
+        setEmail={setEmail}
+      />
+
+      {broker && (
+        <HomeBroker
+          chart={chart}
+          emaEl={emaEl}
+          smaEl={smaEl}
+          toggleEMA={toggleEMA}
+          toggleSMA={toggleSMA}
+        />
+      )}
+    </>
   );
 };
 
